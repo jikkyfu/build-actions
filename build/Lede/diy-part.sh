196193329bc3d2ad94c50983db75abd2a2c7da10
jikkyfu <jikkyfu@gmail.com>
2023-06-16 08:44:25 +0800

    Update diy-part.sh

--- build/Lede/diy-part.sh
+++ build/Lede/diy-part.sh
@@ -101,8 +101,6 @@ openwrt-x86-64-generic-kernel.bin
 openwrt-x86-64-generic.manifest
 openwrt-x86-64-generic-squashfs-rootfs.img.gz
 ipk.tar.gz
-x86-64-generic-rootfs.tar.gz
-x86-64-generic-squashfs-rootfs.img.gz
 EOF
 
 # 在线更新时，删除不想保留固件的某个文件，在EOF跟EOF之间加入删除代码，记住这里对应的是固件的文件路径，比如： rm -rf /etc/config/luci
